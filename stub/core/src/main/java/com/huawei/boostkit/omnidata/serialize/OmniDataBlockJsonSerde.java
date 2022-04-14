80baddb4470049f5f6cd5354e2cc934f6ccd7d0e
xulilililili <1061529620@qq.com>
2022-04-14 16:26:01 +0800

    1.add core model: serialize type

---
+++ stub/core/src/main/java/com/huawei/boostkit/omnidata/serialize/OmniDataBlockJsonSerde.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2020-2021. All rights reserved.
+ */
+
+package com.huawei.boostkit.omnidata.serialize;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import io.airlift.slice.DynamicSliceOutput;
+import io.airlift.slice.SliceOutput;
+import io.airlift.slice.Slices;
+import io.prestosql.spi.block.Block;
+import io.prestosql.spi.block.BlockEncodingSerde;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import static io.hetu.core.transport.block.BlockSerdeUtil.readBlock;
+import static io.hetu.core.transport.block.BlockSerdeUtil.writeBlock;
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Block Json Serde
+ *
+ * @since 2021-07-31
+ */
+public final class OmniDataBlockJsonSerde {
+    private static final int ESTIMATED_SIZE = 64;
+
+    private OmniDataBlockJsonSerde() {}
+
+    /**
+     * Block Json Serializer
+     *
+     * @since 2021-07-31
+     */
+    public static class Serializer extends JsonSerializer<Block> {
+
+
+        public Serializer(BlockEncodingSerde serde) { }
+
+        @Override
+        public void serialize(Block block, JsonGenerator jsonGenerator, SerializerProvider serializerProvider)
+                throws IOException { }
+    }
+
+    /**
+     * Deserializer
+     *
+     * @since 2021-07-31
+     */
+    public static class Deserializer extends JsonDeserializer<Block> {
+
+
+        public Deserializer(BlockEncodingSerde encodingSerde) { }
+
+        @Override
+        public Block deserialize(JsonParser jsonParser, DeserializationContext deserializationContext)
+                throws IOException {
+            return null;
+        }
+    }
+}
+
