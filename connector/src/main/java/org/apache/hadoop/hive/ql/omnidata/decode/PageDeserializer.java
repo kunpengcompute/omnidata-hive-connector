9c3ba86811899c7a38de4ae50eee2f040f60c035
xulilililili <1061529620@qq.com>
2022-05-10 21:09:21 +0800

    clean code

--- connector/src/main/java/org/apache/hadoop/hive/ql/omnidata/decode/PageDeserializer.java
+++ connector/src/main/java/org/apache/hadoop/hive/ql/omnidata/decode/PageDeserializer.java
@@ -19,46 +19,46 @@ import static com.google.common.base.Preconditions.checkArgument;
  */
 public class PageDeserializer implements Deserializer<List<ColumnVector[]>> {
 
-  private final PageDecoding decoding;
+    private final PageDecoding decoding;
 
-  private final DecodeType[] columnTypes;
+    private final DecodeType[] columnTypes;
 
-  public PageDeserializer(DecodeType[] columnTypes) {
-    this.columnTypes = columnTypes;
-    decoding = new PageDecoding();
-  }
-
-  @Override
-  public List<ColumnVector[]> deserialize(SerializedPage page) {
-    if (page.isCompressed() || page.isEncrypted()) {
-      throw new UnsupportedOperationException(
-              "unsupported HiveDeserializer isMarkerPage or compressed or encrypted page ");
+    public PageDeserializer(DecodeType[] columnTypes) {
+        this.columnTypes = columnTypes;
+        decoding = new PageDecoding();
     }
-    SliceInput input = page.getSlice().getInput();
-    int numberOfBlocks = input.readInt();
-    checkArgument(numberOfBlocks >= 0, "decode failed, numberOfBlocks < 0");
-    List<ColumnVector[]> columnVectors = new ArrayList<>();
 
-    for (int i = 0; i < numberOfBlocks; i++) {
-      ColumnVector[] result = decoding.decode(Optional.of(columnTypes[i]), input);
-      if (result == null) {
-        return null;
-      }
-      columnVectors.add(result);
-    }
-    return transform(columnVectors, numberOfBlocks);
-  }
+    @Override
+    public List<ColumnVector[]> deserialize(SerializedPage page) {
+        if (page.isCompressed() || page.isEncrypted()) {
+            throw new UnsupportedOperationException(
+                    "unsupported HiveDeserializer isMarkerPage or compressed or encrypted page ");
+        }
+        SliceInput input = page.getSlice().getInput();
+        int numberOfBlocks = input.readInt();
+        checkArgument(numberOfBlocks >= 0, "decode failed, numberOfBlocks < 0");
+        List<ColumnVector[]> columnVectors = new ArrayList<>();
 
-  private List<ColumnVector[]> transform(List<ColumnVector[]> columnVectors, int numberOfBlocks) {
-    List<ColumnVector[]> newColumnVectors = new ArrayList<>();
-    for (int i = 0; i < columnVectors.get(0).length; i++) {
-      ColumnVector[] result = new ColumnVector[numberOfBlocks];
-      for (int j = 0; j < numberOfBlocks; j++) {
-        result[j] = columnVectors.get(j)[i];
-      }
-      newColumnVectors.add(result);
+        for (int i = 0; i < numberOfBlocks; i++) {
+            ColumnVector[] result = decoding.decode(Optional.of(columnTypes[i]), input);
+            if (result == null) {
+                return null;
+            }
+            columnVectors.add(result);
+        }
+        return transform(columnVectors, numberOfBlocks);
     }
-    return newColumnVectors;
-  }
+
+    private List<ColumnVector[]> transform(List<ColumnVector[]> columnVectors, int numberOfBlocks) {
+        List<ColumnVector[]> newColumnVectors = new ArrayList<>();
+        for (int i = 0; i < columnVectors.get(0).length; i++) {
+            ColumnVector[] result = new ColumnVector[numberOfBlocks];
+            for (int j = 0; j < numberOfBlocks; j++) {
+                result[j] = columnVectors.get(j)[i];
+            }
+            newColumnVectors.add(result);
+        }
+        return newColumnVectors;
+    }
 
 }
